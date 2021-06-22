c0fbd7ec96ef4ad52c0346e7d472a2c502aa5b85
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 19:30:11 -0700

    Initial Commit

---
+++ list/listApp.swift
@@ -0,0 +1,17 @@
+//
+//  listApp.swift
+//  list
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
